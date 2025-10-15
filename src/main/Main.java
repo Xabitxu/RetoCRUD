10b04cf267bd399b611b0290178b4a5009edc9a5
alicia-2004 <aliciaclasegradosuperior@gmail.com>
2025-10-15 16:54:54 +0200

    Commit ventana

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,12 @@ import controller.Controller;
 import java.time.LocalDate;
 import java.util.Map;
 import java.util.TreeMap;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import utilities.Utilities;
 
 /**
@@ -12,8 +18,23 @@ import utilities.Utilities;
  *
  * @author Deusto
  */
-public class Main {
+public class Main extends Application{
 
-    public static void main(String[] args) {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/LogInWindow.fxml"));
+        
+        Scene scene = new Scene(root);
+        stage.setTitle("Login Application");
+        stage.setScene(scene);
+        stage.show();
     }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);        
+    }
+    
 }
